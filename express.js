65ed98a889b4284486e0d96c3775b69752fc66b4
LorenzoLim <renzo-lim@hotmail.com>
2017-11-21 15:32:38 +1000

    CRUD Added to Express

--- express.js
+++ express.js
@@ -3,17 +3,49 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-let students = ['Simon', 'Scott'];
+let id = 1;
+
+let todos = [{
+  task: "Clothes",
+  done: false,
+  id: 0
+},{
+  task: "Laundry",
+  done: true,
+  id: 1
+}];
 
 app.use(bodyParser.json())
 
-app.get('/students', (req,res) =>{
-  res.send(students);
+app.get('/todos', (req,res) =>{
+  res.send(todos);
 });
 
-app.post('/students', (req,res) => {
-  students.push(req.body.name);
-  res.send(students);
+app.get('/todos/:id', (req,res) =>{
+  console.log("GET Todo")
+  res.send(todos[req.params.id]);
 });
+
+app.put('/todos/:id', (req,res) =>{
+  req.body.id = parseInt(req.params.id);
+  todos[req.params.id] = req.body
+  res.send(todos[req.params.id]);
+});
+
+app.delete('/todos/:id', (req, res) => {
+  console.log('DELETE todo')
+  res.send(todo)
+})
+
+
+app.post('/todos', (req,res) => {
+  todos.push(req.body);
+  req.body.id = ++id;
+  res.send(todos);
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
 
 app.listen(port);
